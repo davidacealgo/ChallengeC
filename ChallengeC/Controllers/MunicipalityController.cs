11c3e63a8acd98379a41a911c88abf695519da46
davidacealgo <davidacevedo115@gmail.com>
2019-05-20 13:59:07 -0500

    controller municipio

---
+++ ChallengeC/Controllers/MunicipalityController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChallengeC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChallengeC.Controllers
+{
+    
+    [ApiController]
+    public class MunicipalityController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Search")]
+        public string Search(Municipality municipality)
+        {
+
+        }
+    }
+}
--- ChallengeC/Startup.cs
+++ ChallengeC/Startup.cs
@@ -34,10 +34,8 @@ namespace ChallengeC
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddDbContext<DBContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("ConnectionStr")));
+          
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddTransient<IMunicipality, MunicipalityRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
